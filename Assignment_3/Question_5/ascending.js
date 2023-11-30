c404813702c96e497a7dc02dc3e91ccb834c6ea6
Chaganti1308 <93071195+Chaganti1308@users.noreply.github.com>
2023-11-30 20:15:43 +0530

    Assignment-3  Loops and Conditionals in JavaScript
    Assignment on Loops conditional statements in JavaScript. Assignment No - 3

---
+++ Assignment_3/Question_5/ascending.js
@@ -0,0 +1,8 @@
+// printing numbers from 1 to 25 in ascending order
+
+let num = 1;
+
+while(num<=25){
+    console.log(num);
+    num = num + 1;
+}
---
+++ Assignment_3/Question_5/descending.js
@@ -0,0 +1,8 @@
+// printing numbers from 25 to 1 in descending order
+
+let num = 25;
+
+while(num>0){
+    console.log(num);
+    num = num - 1;
+}
